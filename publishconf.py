63a5888991722e1ea5521e9a027a45df7148f1d6
Rintcius Blok <rintcius@gmail.com>
2013-01-13 23:12:58 +0000

    add pelican cfg and scripts + CNAME for github

---
+++ publishconf.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+import sys
+sys.path.append('.')
+from pelicanconf import *
+
+SITEURL = 'http://blog.rintcius.nl'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+
+# Uncomment following line for absolute URLs in production:
+RELATIVE_URLS = False
+
+GOOGLE_ANALYTICS = 'UA-395810-1'
